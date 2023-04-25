6cd3f0c6016a3d3fafb329b53479b8b1830ddebe
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-25 15:45:09 +0100

    US003 AdminUI updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AdminUI.java
@@ -1,7 +1,9 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 
+import org.w3c.dom.ls.LSOutput;
 import pt.ipp.isep.dei.esoft.project.ui.console.CreateTaskUI;
+import pt.ipp.isep.dei.esoft.project.ui.console.RegisterEmployeeUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.ShowTextUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
@@ -19,10 +21,7 @@ public class AdminUI implements Runnable {
 
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Create Task", new CreateTaskUI()));
-        options.add(new MenuItem("Option 1 ", new ShowTextUI("You have chosen Option A.")));
-        options.add(new MenuItem("Option 2 ", new ShowTextUI("You have chosen Option B.")));
-        options.add(new MenuItem("Option 3 ", new ShowTextUI("You have chosen Option C.")));
+        options.add(new MenuItem("Register a new employee", new RegisterEmployeeUI()));
 
         int option = 0;
         do {
