41db53a81c2f9861204d57be10d1ed114e2683bb
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-29 20:09:17 +0100

    US003 UI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterEmployeeUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterEmployeeUI.java
@@ -1,23 +1,38 @@
 package pt.ipp.isep.dei.esoft.project.ui.console;
 
 
+import pt.ipp.isep.dei.esoft.project.application.controller.RegisterEmployeeController;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
-public class RegisterEmployeeUI {
+public class RegisterEmployeeUI implements Runnable{
 
     String name = "";
     String address = "";
     int phone = 0;
     String email = "";
-    int citizenCardNumber = 0;
-    String postCode = "";
+    int passportCardNumber = 0;
+    int zipCode = 0;
     String city = "";
+    
+    RegisterEmployeeController controller= new RegisterEmployeeController();
 
 
     public void run() {
         name = Utils.readLineFromConsole("Employee name:");
         email = Utils.readLineFromConsole("Employe email:");
         phone = Utils.readIntegerFromConsole("Phone number:");
+        System.out.print("####### Create Address #######");
+        city= Utils.readLineFromConsole("City:");
+        zipCode= Utils.readIntegerFromConsole("zipCode:");
+        address=city+zipCode;
+        System.out.println("Created adress:"+address);
+        System.out.println();
+        controller.RegisterEmployeeController();
+
+
+
+
+
 
 
     }
