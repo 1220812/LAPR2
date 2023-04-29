520830c45bcd336241b6cdafe87d7ea6909a387e
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-30 00:00:20 +0100

    US003 controller updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterEmployeeController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterEmployeeController.java
@@ -3,15 +3,23 @@ package pt.ipp.isep.dei.esoft.project.application.controller;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 import pt.ipp.isep.dei.esoft.project.repository.agencyRepository;
 import pt.ipp.isep.dei.esoft.project.repository.roleRepository;
-
-
-import java.util.List;
+import pt.ipp.isep.dei.esoft.project.repository.storeRepository;
 
 public class RegisterEmployeeController {
     public void RegisterEmployeeController() {
         Repositories repositories = Repositories.getInstance();
+
+
+    }
+
+    public void getRoleRepository(){
         roleRepository.getEmployeeRoles();
+    }
+    public void getAgencyRepository(){
         agencyRepository.getAgencies();
+    }
+    public void getStoreRepository(){
+        storeRepository.getStores();
     }
 
 }
