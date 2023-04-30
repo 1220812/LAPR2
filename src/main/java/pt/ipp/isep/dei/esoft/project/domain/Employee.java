8b691a1e868be04ac09515f4537fb092d53ad369
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-30 04:59:02 +0100

    US003 Employee updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
@@ -8,6 +8,13 @@ public class Employee {
     private int phoneNumber;
     private int passportCardNumber;
     private int taxNumber;
+    private String role;
+    private String Address;
+
+    private String agency;
+
+    private String store;
+    private String pass;
 
 
     private static final String DEFAULT_NOME = "No name";
@@ -16,14 +23,36 @@ public class Employee {
     private static final int DEFAULT_PASSPORTNUMBER = 0;
     private static final int DEFAULT_TAXNUMBER = 0;
 
+    private static final String DEFAULT_ROLE = "No role";
 
-    public Employee(String name, String emailAddress, int phoneNumber, int PassportCardNumber, int taxNumber) {
+    private static final String DEFAULT_AGENCY = "No agency";
+    private static final String DEFAULT_STORE = "No store";
+    private static final String DEFAULT_ADDRESS = "No passport";
+
+    private static final String DEFAULT_PASSWORD = "No password";
+
+
+    public Employee(String name, String emailAddress, int phoneNumber, int passportCardNumber, int taxNumber, String address, String role, String agency, String store, String pass) {
         this.name = name;
         this.email = emailAddress;
         this.phoneNumber = phoneNumber;
         this.passportCardNumber = passportCardNumber;
         this.taxNumber = taxNumber;
+        this.Address = address;
+        this.role = role;
+        this.agency = role;
+        this.store = role;
+        this.pass = pass;
     }
+
+
+    public Employee(String email) {
+        this.email = email;
+    }
+
+
+
+
 
 
     public Employee() {
@@ -32,7 +61,18 @@ public class Employee {
         this.phoneNumber = DEFAULT_PHONENUMBER;
         this.passportCardNumber = DEFAULT_PASSPORTNUMBER;
         this.taxNumber = DEFAULT_TAXNUMBER;
+        this.Address = DEFAULT_ADDRESS;
+        this.role = DEFAULT_ROLE;
+        this.agency=DEFAULT_AGENCY;
+        this.store=DEFAULT_STORE;
+        this.pass=DEFAULT_PASSWORD;
     }
+
+    public static Employee newEmployee(String name, String email, int phoneNumber, int passportCardNumber, int taxNumber, String address, String role, String agency, String store, String pass) {
+        Employee newEmployee= new Employee(name, email, phoneNumber, passportCardNumber, taxNumber, address, role, agency, store, pass);
+        return newEmployee;
+    }
+
 
     public String getName() {
         return name;
@@ -75,6 +115,62 @@ public class Employee {
         this.taxNumber = taxNumber;
     }
 
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", passportCardNumber=" + passportCardNumber +
+                ", taxNumber=" + taxNumber +
+                ", role='" + role + '\'' +
+                ", Address='" + Address + '\'' +
+                ", agency='" + agency + '\'' +
+                ", store='" + store + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -101,4 +197,5 @@ public class Employee {
     public Employee clone() {
         return null;
     }
+
 }
