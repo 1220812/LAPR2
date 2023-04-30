1374ca0d7c69e1d9289d86a6ef2d92613fb7c0e8
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-30 05:01:13 +0100

    US003 controller updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterEmployeeController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterEmployeeController.java
@@ -1,11 +1,9 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
+import pt.ipp.isep.dei.esoft.project.domain.EmailService;
 import pt.ipp.isep.dei.esoft.project.domain.Employee;
 import pt.ipp.isep.dei.esoft.project.domain.PasswordGenerator;
-import pt.ipp.isep.dei.esoft.project.repository.Repositories;
-import pt.ipp.isep.dei.esoft.project.repository.agencyRepository;
-import pt.ipp.isep.dei.esoft.project.repository.roleRepository;
-import pt.ipp.isep.dei.esoft.project.repository.storeRepository;
+import pt.ipp.isep.dei.esoft.project.repository.*;
 
 public class RegisterEmployeeController {
     public void RegisterEmployeeController() {
@@ -14,19 +12,30 @@ public class RegisterEmployeeController {
 
     }
 
-    public void getRoleRepository(){
+    public void getRoleRepository() {
         roleRepository.getEmployeeRoles();
     }
-    public void getAgencyRepository(){
+
+    public void getAgencyRepository() {
         agencyRepository.getAgencies();
     }
-    public void getStoreRepository(){
+
+    public void getStoreRepository() {
         storeRepository.getStores();
     }
 
-    public Employee RegisterEmployee(String name, String address, int phone, String email, int passportCardNumber, String s, String inputRole, String inputAgency, String inputStore){
-        PasswordGenerator.generatePassword();
-        return null;
+    public Employee RegisterEmployee(String name, String address, int phone, String email, int passportCardNumber, int taxNumber, String role, String agency, String store, String pass) {
+        pass = PasswordGenerator.generatePassword();
+        return Employee.newEmployee(name, email, phone, passportCardNumber, taxNumber, address, role, agency, store, pass);
     }
+
+    public void getUserRepository() {
+         userRepository.getUserList();
+    }
+    public void sendRegisteredUserEmail(String email, String pass){
+        pass = PasswordGenerator.generatePassword();
+        EmailService.sendEmail(email, pass);
+    }
+
 
 }
