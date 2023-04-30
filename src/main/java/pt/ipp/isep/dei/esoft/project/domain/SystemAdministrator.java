25e1956972d31071a83aafbf0076fcae4ff23784
Francisco Silveira <1220813@isep.ipp.pt>
2023-04-30 05:01:56 +0100

    US003 System Administrator updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/SystemAdministrator.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/SystemAdministrator.java
@@ -1,13 +1,13 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 public class SystemAdministrator extends Employee {
-    public SystemAdministrator(String name, String email, int phoneNumber, int passportCardNumber, int taxNumber) {
-        super(name, email, phoneNumber, passportCardNumber, taxNumber);
+    public SystemAdministrator(String name, String email, int phoneNumber, int passportCardNumber, int taxNumber, String role, String agency, String store, String address, String pass ) {
+        super(name, email, phoneNumber, passportCardNumber, taxNumber, role, agency, store, address, pass );
 
     }
     @Override
     public Employee clone() {
-        return new SystemAdministrator(super.getName(),super.getEmail(),super.getPhoneNumber(),super.getPassportCardNumber(),super.getTaxNumber());
+        return new SystemAdministrator(super.getName(),super.getEmail(),super.getPhoneNumber(),super.getPassportCardNumber(),super.getTaxNumber(), super.getRole(), super.getAgency(), super.getStore(), super.getAddress(), super.getPass());
     }
 
 }
