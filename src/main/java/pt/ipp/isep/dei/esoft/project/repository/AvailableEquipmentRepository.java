f0800983cba7f2d235ace067ed312f20096ec083
Diogo_1220812 <1220812@isep.ipp.pt>
2023-05-21 22:00:44 +0100

    Updates in some classes and repositories

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/AvailableEquipmentRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/AvailableEquipmentRepository.java
@@ -4,21 +4,31 @@ import pt.ipp.isep.dei.esoft.project.domain.AvailableEquipment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AvailableEquipmentRepository {
     private List<AvailableEquipment> availableEquipmentList = new ArrayList<>();
 
-    public void addAvailableEquipment(AvailableEquipment availableEquipment){
-        if(validateAvailableEquipment(availableEquipment))
-            availableEquipmentList.add(availableEquipment);
+    public List<AvailableEquipment> addAvailableEquipment(AvailableEquipment availableEquipment){
+        availableEquipmentList.add(availableEquipment);
+        Optional<AvailableEquipment> newEquipment = Optional.empty();
+        boolean operationSuccess = false;
+        if (validateAvailableEquipment(availableEquipment)) {
+            newEquipment = Optional.of((AvailableEquipment) availableEquipment.clone());
+            operationSuccess = availableEquipmentList.add((AvailableEquipment) newEquipment.get());
+        }
+
+        if (!operationSuccess) {
+            newEquipment = Optional.empty();
+        }
+        return availableEquipmentList;
     }
 
     public boolean validateAvailableEquipment(AvailableEquipment availableEquipment) {
-        return !this.availableEquipmentList.contains(availableEquipment);
+        boolean isValid = !availableEquipmentList.contains(availableEquipment);
+        return isValid;
     }
-
     public List<AvailableEquipment> getavailableEquipmentList(){
         return List.copyOf(this.availableEquipmentList);
     }
-
 }
