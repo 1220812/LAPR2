fd4fe3b3e44b50abc3e902af16530e8716d4ae01
Francisco Silveira <1220813@isep.ipp.pt>
2023-05-21 21:53:51 +0100

    RequestTypeRepository updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/RequestTypeRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/RequestTypeRepository.java
@@ -4,6 +4,7 @@ import pt.ipp.isep.dei.esoft.project.domain.RequestType;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RequestTypeRepository {
     private final List<RequestType> requestTypes = new ArrayList<>();
@@ -17,14 +18,33 @@ public class RequestTypeRepository {
      * @param requestType request type to add
      * @return request type list with new one added
      */
-    public List<RequestType> add(RequestType requestType){
-        getRequestTypeList().add(requestType);
-        return requestTypes;
-    }
+
     public boolean validateRequestType(RequestType requestType){
         return !this.requestTypes.contains(requestType);
     }
     public List<RequestType> getRequestTypes(){
         return List.copyOf(this.requestTypes);
+    }
+
+    public List<RequestType> add(RequestType requestType) {
+
+        requestTypes.add(requestType);
+
+        Optional<RequestType> newRequestType = Optional.empty();
+        boolean operationSuccess = false;
+
+        if (validateRequestType(requestType)) {
+            newRequestType = Optional.of((RequestType) requestType.clone());
+            operationSuccess = requestTypes.add((RequestType) newRequestType.get());
+        }
+
+        if (!operationSuccess) {
+            newRequestType = Optional.empty();
+        }
+        return requestTypes;
+    }
+    private boolean validateEquipment(RequestType requestType) {
+        boolean isValid = !requestTypes.contains(requestType);
+        return isValid;
     }
 }
