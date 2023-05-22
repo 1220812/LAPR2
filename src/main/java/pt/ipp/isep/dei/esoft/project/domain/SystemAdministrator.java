bfe77864a156299bee4305189ce1eed00fd0c763
Francisco Silveira <1220813@isep.ipp.pt>
2023-05-22 16:29:56 +0100

    SystemAdministrator updated

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/SystemAdministrator.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/SystemAdministrator.java
@@ -1,7 +1,7 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 public class SystemAdministrator extends Employee {
-    public SystemAdministrator(String name, String email, String phoneNumber, PassportCardNumber passportCardNumber, TaxNumber taxNumber, String role, Address address, String pass ) {
+    public SystemAdministrator(String name, String email, String phoneNumber, PassportCardNumber passportCardNumber, TaxNumber taxNumber, Role role, Address address, String pass ) {
         super(name, email, phoneNumber, passportCardNumber, taxNumber, address, role, pass);
     }
     @Override
