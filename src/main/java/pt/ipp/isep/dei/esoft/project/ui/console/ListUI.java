37d5181066bba1b626c315e8967db31f8f5f424b
Francisco Silveira <1220813@isep.ipp.pt>
2023-05-30 11:20:44 +0100

    US015 ListController and UI

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/ListUI.java
@@ -0,0 +1,18 @@
+package pt.ipp.isep.dei.esoft.project.ui.console;
+
+import pt.ipp.isep.dei.esoft.project.application.controller.ListController;
+import pt.ipp.isep.dei.esoft.project.application.controller.RegisterEmployeeController;
+
+public class ListUI implements Runnable{
+
+    ListController controller = new ListController();
+
+
+
+    public void run() {
+
+
+
+        
+    }
+}
