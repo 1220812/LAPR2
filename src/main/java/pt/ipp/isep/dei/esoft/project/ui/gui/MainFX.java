92ca7fe418edacd3fe5f7018b125d3121d33767c
Diogo_1220812 <1220812@isep.ipp.pt>
2023-06-06 12:08:45 +0100

    Implementing the javafx

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainFX.java
@@ -0,0 +1,24 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class MainFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
