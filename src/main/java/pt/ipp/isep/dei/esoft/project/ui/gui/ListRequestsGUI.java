190d128cc575e2c3ffa3134942d0fe02170039b1
Francisco Silveira <1220813@isep.ipp.pt>
2023-06-17 18:40:52 +0100

    JavaFx fixed

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ListRequestsGUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ListRequestsGUI.java
@@ -3,6 +3,8 @@ package pt.ipp.isep.dei.esoft.project.ui.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -16,12 +18,31 @@ public class ListRequestsGUI extends Application implements Runnable {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(new File("src/main/resources/fxml/us015.fxml").toURL());
+        FXMLLoader fxmlLoader1 = new FXMLLoader(new File("src/main/resources/fxml/us015.fxml").toURL());
         Scene scene = new Scene(fxmlLoader.load());
+        Pane pane = fxmlLoader1.load();
 
-        stage.setTitle("US015");
+        Image iconImage = new Image(("list2.jpg"));
+
+
+
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image("Icon.jpg"),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
+        Background background = new Background(backgroundImage);
+        pane.setBackground(background);
+
+        stage.getIcons().add(iconImage);
+
+        stage.setTitle("Projeto Integrador");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
+
+
     }
 
 }
